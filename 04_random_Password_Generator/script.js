3755490401f3c163465f4843d80a728788f7384f
Yuvraj-Singh-Rajawat <yuvraj123singhrajawat@gmail.com>
2024-06-19 22:41:04 +0530

    add more features in Project 4

--- 04_random_Password_Generator/script.js
+++ 04_random_Password_Generator/script.js
@@ -1,19 +1,43 @@
 const inputElement = document.getElementById('password')
 const generateBtn = document.getElementById('generateBtn')
 const copyBtn = document.getElementById('copyBtn')
-// let passwordLength = 10;
+const listContainer = document.getElementById('list-container')
+const liElements = listContainer.getElementsByTagName('li');
+
+function toggleChecked(li) {
+    const isChecked = li.getAttribute('data-checked') === 'true';
+    li.setAttribute('data-checked', !isChecked);
+    if (!isChecked) {
+        li.classList.add('checked');
+    } else {
+        li.classList.remove('checked');
+    }
+}
+
 generateBtn.addEventListener('click', ()=>{
     let randomPasswordLength = Math.floor(Math.random()*10)+10
+    let uppercaseEl = document.getElementById('uppercase')
+    let numberEl = document.getElementById('number')
+    let symbolEl = document.getElementById('Symbols')
+
+    const isUppercase = uppercaseEl.getAttribute('data-checked') === 'true';
+    const isSymbol = symbolEl.getAttribute('data-checked') === 'true';
+    const isNumber = numberEl.getAttribute('data-checked') === 'true';
 
     // inputElement.innerHTML = ""
-    const pass = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#%^&*()-_+=;~`1234567890'
-    let passlength = pass.length;
-    let password = '';
+    const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercase = isUppercase? "ABCDEFGHIJKLMNOPQRSTUVWXYZ" : ""
+    const number = isNumber ? "0123456789": ""
+    const symbols = isSymbol ? "`~!@#$%^&*()-_=+" : ""
+
+    const passwordChar = lowercase + uppercase + number + symbols
+    let passlength = passwordChar.length;
+    let password = ""
 
     for (let i = 0; i < randomPasswordLength; i++) {
         const randomIndex = Math.floor(Math.random()*passlength)
         // console.log(randomIndex);
-        password += pass[randomIndex]
+        password += passwordChar[randomIndex]
     }
 
     inputElement.value = password;
@@ -29,5 +53,4 @@ copyBtn.addEventListener('click',() => {
         navigator.clipboard.writeText(inputElement.value);
     }
     inputElement.select();
-    // alert("Password Copied")
 })
